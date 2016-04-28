84e8fd3d2fec09cf23f68588a0758f92a133bafb
jeremylokekm <jeremylokekm@gmail.com>
2016-04-28 19:58:36 +0800

    Add first banner ad

---
+++ app/src/main/java/com/example/android/myadapp/BannerActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.myadapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class BannerActivity extends Activity{
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banner);
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+        mAdView.loadAd(adRequest);
+    }
+}
